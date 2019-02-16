0bff18cafd14a089e741dd24c6b7abfd835606f3
Andy <qingjielu990916@gmail.com>
2019-02-16 14:39:23 -0500

    user and balance display up

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      balance: 0
+    };
+  }
+  async componentWillMount() {
+    const res = await axios.get("auth/current_user");
+    const user = res.data;
+    if (!user) {
+      console.log("redirecting");
+      this.props.history.push("/");
+    }
+    this.setState({ username: user.username, balance: user.balance });
+  }
+
   render() {
     return (
       <div>
-        <h1>Main page to be Implemented with ethereum</h1>
+        <h1>Hello {this.state.username}</h1>
+        <p>Your current balance is {this.state.balance} dollars</p>
+
         <a href="auth/logout">logout</a>
+        <button>Transfer Money</button>
+        <button>Withdraw</button>
       </div>
     );
   }
--- models/users.js
+++ models/users.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  balance: Number
 });
 
 mongoose.model("users", userSchema);
