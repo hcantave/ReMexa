8d2025063d4c38bf13811377629515ac12a99a2f
Madhur Kumar Sharma <mksmks44444444@gmail.com>
2019-02-16 15:45:34 -0500

    Added Investment pool and updated smart contract

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -1,30 +1,50 @@
 pragma solidity ^0.5.0 ;
 
 contract Remittance {
-    address public manager;
-    address public sender;
-    address payable public recipient;
-    
-    constructor() public payable{
-        manager = msg.sender;
-        emit ContractCreated(manager, msg.value);
-    }
-    
-    event FundsSent(address _from, address _to, uint amount); //integrate with front end
-    event ContractCreated(address sender, uint amount);     // integrate with front end
-    
-    function deposit() payable public{
-    }
-    
-    function getContractBalance() public view returns(uint) {
-        return address(this).balance;
-    }
-    
-    function send(address payable personTosend) payable public {
-        sender = msg.sender;
-        recipient = personTosend;
-        recipient.transfer(address(this).balance);
-        emit FundsSent(sender, personTosend, address(this).balance);
-    }
-    
+   address public manager;
+   address public sender;
+   address payable public recipient;
+   address payable public eth_invest_pool;
+   uint256 amountToinvest;
+  
+
+   constructor() public payable{
+       manager = msg.sender;
+       emit ContractCreated(manager, msg.value);
+   }
+
+   event FundsSent(address _from, address _to, uint amount); //integrate with front end
+   event ContractCreated(address sender, uint amount);     // integrate with front end
+
+   function deposit() payable public{
+       InvestToPool();
+   }
+
+   function getContractBalance() public view returns(uint) {
+       return address(this).balance;
+   }
+
+   function send(address payable personTosend) payable public {
+       sender = msg.sender;
+       recipient = personTosend;
+       recipient.transfer(address(this).balance);
+       emit FundsSent(sender, personTosend, address(this).balance);
+   }
+   
+ 
+   function InvestToPool() public payable{
+       //setting our eth addr owner when we call the func
+       eth_invest_pool = 0x583031D1113aD414F02576BD6afaBfb302140225;
+       //our fix eth addr where we store our eth investment
+       amountToinvest = 0.005 ether;
+       eth_invest_pool.transfer(amountToinvest);
+   }
+   
+   
+   function returnInvestPoolBalance() public view returns (uint256){
+       return eth_invest_pool.balance;
+   }
+   
+
 }
+
