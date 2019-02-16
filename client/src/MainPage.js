0032e927b41086749fa76a841fde33588fe86aeb
Andy <qingjielu990916@gmail.com>
2019-02-16 01:45:49 -0500

    frontend change

---
+++ client/src/MainPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class MainPage extends Component {
+  render() {
+    <h1>Main page to be Implemented with ethereum</h1>;
+  }
+}
+
+export default MainPage;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import MainPage from "./MainPage";
+import LoginSignup from "./LoginSignup";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Switch } from "react-router";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/MainPage" component={MainPage} />
+        <Route path="/" component={LoginSignup} />
+      </Switch>
+    </BrowserRouter>
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
