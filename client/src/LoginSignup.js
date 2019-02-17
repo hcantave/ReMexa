8e8cdbd80bbbe6957b0f0838a599cb663121a889
Evan-Hoffman <evanhoffman370@gmail.com>
2019-02-17 00:16:18 -0500

    fix

--- client/src/LoginSignup.js
+++ client/src/LoginSignup.js
@@ -89,7 +89,7 @@ class LoginSignup extends Component {
   render() {
     return (
       <style>
-        body {
+        .center {
           margin: auto;
           width: 60%;
           border:  3px solid #73AD21;
