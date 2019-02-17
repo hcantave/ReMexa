90bb65f6134fb461c1d1dd27a62096de67815715
Madhur Kumar Sharma <mksmks44444444@gmail.com>
2019-02-17 10:16:29 -0500

    changes

--- client/src/LoginSignup.js
+++ client/src/LoginSignup.js
@@ -89,6 +89,7 @@ class LoginSignup extends Component {
   render() {
     return (
       <div className="container" className ="col-md-4 col-md-offset-4" id="wrap">
+        <img src={require('/mexicoflag.png')} height="60" width="60"></img>
         <form onSubmit={this.onSubmitLogin} id="login-page">
         <h2 id="intro-head"> ReMexa </h2>
         <Spacer amount={8} />
@@ -156,7 +157,6 @@ class LoginSignup extends Component {
             required 
           />
           </div>
-            <img src="mexicoflag.png" height="30" width="30"></img>
           <br></br>
           <div id="loginbutton">
           <button type="submit" className="btn btn-success btn-lg" id="button">Signup</button>
