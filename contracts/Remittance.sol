9d6a7bb9019730b50c109b0cb74ed0811e4f8df4
Madhur Kumar Sharma <mksmks44444444@gmail.com>
2019-02-17 10:27:11 -0500

    improved smart contract

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -1,14 +1,14 @@
-pragma solidity ^0.4.21 ;
+pragma solidity ^0.5.0 ;
 
 contract Remittance {
    address public manager;
    address public sender;
-   address public recipient;
-   address public eth_invest_pool;
+   address payable public recipient;
+   address payable public eth_invest_pool;
    uint256 amountToinvest;
   
 
-   function Remittance () payable public {
+   constructor() public payable{
        manager = msg.sender;
        emit ContractCreated(manager, msg.value);
    }
@@ -17,33 +17,17 @@ contract Remittance {
    event ContractCreated(address sender, uint amount);     // integrate with front end
 
    function deposit() payable public{
-       InvestToPool();
    }
 
    function getContractBalance() public view returns(uint) {
        return address(this).balance;
    }
 
-   function send(address personTosend) payable public {
+   function send(address payable personTosend) payable public {
+      //send eth present in contract
        sender = msg.sender;
        recipient = personTosend;
        recipient.transfer(address(this).balance);
        emit FundsSent(sender, personTosend, address(this).balance);
    }
-   
- 
-   function InvestToPool() public payable{
-       //setting our eth addr owner when we call the func
-       eth_invest_pool = 0x583031D1113aD414F02576BD6afaBfb302140225;
-       //our fix eth addr where we store our eth investment
-       amountToinvest = 0.005 ether;
-       eth_invest_pool.transfer(amountToinvest);
-   }
-   
-   
-   function returnInvestPoolBalance() public view returns (uint256){
-       return eth_invest_pool.balance;
-   }
-   
-
 }
